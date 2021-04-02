26f3df9b102865a67dce423463c984edad2bc876
Holly <holly_cooper@live.com.au>
2021-04-02 21:25:40 +1000

    landing

--- app.py
+++ app.py
@@ -3,4 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route()
+@app.errorhandler(404)
+def page_error(e):
+    print(e, "\n\n")
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
